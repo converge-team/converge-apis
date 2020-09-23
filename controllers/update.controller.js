473f0bf813e2e8715da80574e559b44aa26cb04e
Judge Godwins <judgegodwins@gmail.com>
2020-09-23 10:17:35 +0100

    fix error

--- controllers/update.controller.js
+++ controllers/update.controller.js
@@ -1,8 +1,33 @@
 
-const User = require('../model/User');
+const cloudinary = require('cloudinary').v2
 
-exports.updatePhoto = (req, res) => {
+const User = require('../model/User');
+const objectify = require('../utils/objectify');
+const Response = require('../utils/response')
+const sockets = require('../socket.server').sockets
+
+exports.updatePhoto = async (req, res) => {
+    const { email, username } = req.user
     const { file } = req;
 
-    console.log('file: ',file);
+    const userSocket = sockets.find(socket => socket.username === username);
+
+    const uploadedPhoto = await cloudinary.uploader.upload(file.path);
+    User.findOneAndUpdate({ email }, {profile_photo_url: uploadedPhoto.secure_url}, { new: true, useFindAndModify: false })
+        .then(user => {
+
+            userSocket.broadcast.emit('new_profile_photo',
+                
+                { url: user.profile_photo_url, from: userSocket.userId}
+            )
+
+            Response.success({ 
+                res,
+                message: 'Profile photo updated',
+                data: {
+                    user: objectify(user, true)
+                }
+            })
+
+        })
 }
